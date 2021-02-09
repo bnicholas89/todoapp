776dc4d90d181490db20404e5c0279ba2f65d251
Nicholas Bates <nbates66@gmail.com>
2021-02-09 14:39:45 -0800

    git initialization

---
+++ app.py
@@ -0,0 +1,41 @@
+from flask import Flask, render_template, request, redirect, url_for, jsonify
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://nicholasbates@localhost:5432/todoapp'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+# Our Todo class is the model and the index.html is the view
+
+
+class Todo(db.Model):
+    __tablename__ = 'todos'
+    id = db.Column(db.Integer, primary_key=True)
+    description = db.Column(db.String(), nullable=False)
+
+    def __repr__(self):
+        return f'<Todo {self.id} {self.description}>'
+
+
+# index is the controller in this app
+
+
+@app.route('/todos/create', methods=['POST'])
+def create_todo():
+    description = request.get_json()['description']
+    todo = Todo(description=description)
+    db.session.add(todo)
+    db.session.commit()
+    return jsonify({
+        'description': todo.description
+    })
+
+
+@app.route('/')
+def index():
+    return render_template('index.html', data=Todo.query.all())
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
