fb83c482259e6ffe2d84500c0cac21910dfd501c
Nicholas Bates <nbates66@gmail.com>
2021-02-13 12:21:50 -0800

    pip install flask migrate, import Migrate, def upgrade script changes to allow null value for completed column then update column to insert false where completed is null. Post update altered completed column to set nullable constraint back to false.

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
+import sys
 from flask import Flask, render_template, request, redirect, url_for, jsonify, abort
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://nicholasbates@localhost:5432/todoapp'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 # Our Todo class is the model and the index.html is the view
 
@@ -13,6 +17,7 @@ class Todo(db.Model):
     __tablename__ = 'todos'
     id = db.Column(db.Integer, primary_key=True)
     description = db.Column(db.String(), nullable=False)
+    completed = db.Column(db.Boolean, nullable=False, default=False)
 
     def __repr__(self):
         return f'<Todo {self.id} {self.description}>'
@@ -24,21 +29,23 @@ class Todo(db.Model):
 @app.route('/todos/create', methods=['POST'])
 def create_todo():
     error = False
+    body = {}
     try:
         description = request.get_json()['description']
         todo = Todo(description=description)
         db.session.add(todo)
         db.session.commit()
+        body['description'] = todo.description
     except:
         error = True
         db.session.rollback()
         print(sys.exc_info())
     finally:
         db.session.close()
-    if not error:
-        return jsonify({
-            'description': todo.description
-        })
+    if error:
+        abort(400)
+    else:
+        return jsonify(body)
 
 
 @app.route('/')
