71775ea1d9b610450b86fc4f83c94532817b7c52
Nicholas Bates <nbates66@gmail.com>
2021-02-14 16:02:28 -0800

    fixed server code(app.py) @app.routes to work

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ class Todo(db.Model):
     __tablename__ = 'todos'
     id = db.Column(db.Integer, primary_key=True)
     description = db.Column(db.String(), nullable=False)
-    completed = db.Column(db.Boolean, nullable=False)
+    completed = db.Column(db.Boolean, default=False)
     list_id = db.Column(db.Integer, db.ForeignKey(
         'todolists.id'), nullable=False)
 
@@ -58,11 +58,12 @@ def create_todo():
     body = {}
     try:
         description = request.get_json()['description']
-        todo = Todo(description=description, completed=False)
+        list_id = request.get_json()['list_id']
+        todo = Todo(description=description)
+        active_list = TodoList.query.get(list_id)
+        todo.list = active_list
         db.session.add(todo)
         db.session.commit()
-        body['id'] = todo.id
-        body['completed'] = todo.completed
         body['description'] = todo.description
     except:
         error = True
@@ -70,10 +71,10 @@ def create_todo():
         print(sys.exc_info())
     finally:
         db.session.close()
-    if error:
-        abort(400)
-    else:
+    if not error:
         return jsonify(body)
+    else:
+        abort(500)
 
 
 @app.route('/todos/<todo_id>/set-completed', methods=['POST'])
@@ -91,9 +92,17 @@ def set_completed_todo(todo_id):
     return redirect(url_for('index'))
 
 
+@app.route('/lists/<list_id>')
+def get_list_todos(list_id):
+    return render_template('index.html',
+                           lists=TodoList.query.all(),
+                           active_list=TodoList.query.get(list_id),
+                           todos=Todo.query.filter_by(list_id=list_id).order_by('id').all())
+
+
 @app.route('/')
 def index():
-    return render_template('index.html', todos=Todo.query.order_by('id').all())
+    return redirect(url_for('get_list_todos', list_id=1))
 
 
 if __name__ == '__main__':
